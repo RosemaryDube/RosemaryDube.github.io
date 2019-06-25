98d213025bbc1bd3c114a2dba6047327053d6bf4
Rose Dube <rosemary52.rd@gmail.com>
2019-06-25 18:46:29 -0400

    Added files

---
+++ project.js
@@ -0,0 +1,10 @@
+// // var name = "Rosemary";
+// // alert("hello " + name);
+// var arrow = document.querySelector(".arrow");
+// var target = document.querySelector(".target");
+//
+// arow.style.right = "130px";
+// target.addEventlistiner("click", function(){
+//
+//   arrow.style.right ="130px";
+// }
